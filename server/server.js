65f9da73f89f2a08cc381e6d272c40a3d2cb74bd
DEBASHISHSAHOO285 <sahoodebashish285@gmail.com>
2026-04-10 23:53:56 +0530

    fix route error

--- server/server.js
+++ server/server.js
@@ -237,8 +237,8 @@ const path = require("path");
 // serve frontend
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // START
